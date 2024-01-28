bc1c55f472d18d1448a8c9cb307d6711b4216855
Islomjon Abduganiyev <islomjonabduganiyev@Islomjons-MacBook-Air.local>
2024-01-28 22:36:10 +0500

    task complated

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './assets/header'
+import Main from './assets/main'
+
+function App() {
+
+  return (
+    <>
+      <div className="card">
+        <Header></Header>
+        <Main></Main>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/animal_card/main/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./index.css"
+
+export default function Main() {
+  return (
+    <>
+      <div className="main">
+        <div className="main__text">
+            <h2>Love and kindness are never wasted.</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+            <button>Join Us</button>
+        </div>
+        <div className="main__img">
+        </div>
+      </div>
+    </>
+  )
+}
